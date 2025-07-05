3497263781c3a35fe5fcc6d155093b2ffd555581
priyadarsini <piu270394@gmail.com>
2025-07-05 14:11:36 +0530

    it's about variable

---
+++ variable.py
@@ -0,0 +1,16 @@
+name= 'youtube'
+print (name)
+
+print (name [0])
+print (name [-1])
+print (name[-3])
+
+print (name[0:2])
+print (name [:4])
+print (name[0:3])
+
+# now we will try to change the name youtube to mytube
+
+# name[0:3] ='my'   #it's impossible we cannot change
+
+print('my'+name[3:])
