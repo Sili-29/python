e5e38f1c8459032d4675f1aabfec8fe334941dfd
priyadarsini <piu270394@gmail.com>
2025-07-09 22:08:17 +0530

    user interaction

---
+++ user_interaction.py
@@ -0,0 +1,20 @@
+#Taking input from user
+a= input('Enter your name : ')
+print('Welcome {a}')
+print(f'Welcome {a}')
+
+# f= it tells Python to evaluate any expressions inside curly braces {} and insert their values into the string.
+
+
+num1= input("enter a number : ")
+num2= input("enter 2nd number : ")
+result= num1+num2
+print(result)
+# num1 and num2 are string here "" so we need  to change the type to integer. it is called as type casting
+
+n1=int(input("1st number : "))
+n2=int(input("2nd number : "))
+sum=n1+n2
+print(sum)
+
+# now it will add the values
